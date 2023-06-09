5cacb2e364092b9bf7d0eed511f2a4d3bf074e35
Cristian Copolutti <cristian.copolutti97@gmail.com>
2023-06-09 15:32:08 +0200

    Esercizi PHP

---
+++ Esercizi PHP/0906.php
@@ -0,0 +1,49 @@
+<?php
+
+//prima funzione --> +
+/*
+
+numero1
+numero2
+
+funzione somma (numero1, numero2){
+    return numero1 + numero2
+}
+
+
+funzione sottrazione(numero1, numero2){
+    return numero1 - numero2
+}
+
+
+funzione moltiplicazione(numero1, numero2){
+    return numero1 * numero2
+}
+
+funzione divisione(numero1, numero2){
+    return numero1 / numero2
+}
+
+
+echo somma(3,4);
+echo sottrazione(7,2);
+echo moltiplicazione(3,9)
+echo divisione(8,4)
+
+*/
+
+function somma(int $a, int $b){
+    return $a + $b;
+}
+
+echo(somma(5,4));
+
+function sottrazione()
+
+function moltiplicazione()
+
+function divisione()
+
+
+
+?>
