f7b61ab85f0061eb7babcd30a535906ec66db03b
Cristian Copolutti <cristian.copolutti97@gmail.com>
2023-06-16 12:12:22 +0200

    esercizi webinar 16.06

---
+++ Esercizi PHP/palindroma.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Esercizio parola palindroma</title>
+        <style>
+        </style>
+    </head>
+    <body>
+        <?php
+        ?>
+    <h1>Verifica se la parola è palindroma:</h1>
+    <form>
+        <label for="word">Parola:</label>
+        <input type="text" name="word" id="word" required>
+        <input type="submit" class="button" name="verificaPalindroma" value="Verifica">
+    </form>
+    <body>
+</html>
